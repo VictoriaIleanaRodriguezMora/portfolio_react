279153224d544c89c506a519bfd19e4a7574cca6
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2024-03-27 14:04:58 -0300

    feat(components): :sparkles: Alternativa de Lista Punteada

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Lista_Punteada from "./components/Lista_Punteada/Lista_Punteada";
+import ALTERNATIVA_Lista_Punteada from "./components/ALTERNATIVA-Lista_Punteada/ALTERNATIVA-Lista_Punteada";
 
 function App() {
   return (
     <>
       <Lista_Punteada />
+      <ALTERNATIVA_Lista_Punteada/>
     </>
   );
 }
---
+++ src/components/ALTERNATIVA-Lista_Punteada/ALTERNATIVA-Lista_Punteada.tsx
@@ -0,0 +1,38 @@
+// import React from "react";
+import "./ALTERNATIVA-Lista_Punteada.css";
+
+const ALTERNATIVA_Lista_Punteada = () => {
+  return (
+    <>
+      <ul>
+        <li>
+          <span></span>
+          <div>
+            <p> FEBRUARY, 2022 - JULY, 2023 </p>
+            <h2> Front-end Developer @ Never8 </h2>
+            <p>
+              Maintaining and developing new features for a React.js project, as
+              well as integrating its functionality with a REST API are my
+              primary responsibilities as a Developer.
+            </p>
+          </div>
+        </li>
+
+        <li>
+          <span></span>
+          <div>
+            <p> FEBRUARY, 2022 - JULY, 2023 </p>
+            <h2> Front-end Developer @ Never8 </h2>
+            <p>
+              Maintaining and developing new features for a React.js project, as
+              well as integrating its functionality with a REST API are my
+              primary responsibilities as a Developer.
+            </p>
+          </div>
+        </li>
+      </ul>
+    </>
+  );
+};
+
+export default ALTERNATIVA_Lista_Punteada;
