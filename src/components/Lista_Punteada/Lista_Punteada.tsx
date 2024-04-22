f838cd1af0fa6a8c43532cfa5d7e699ba20bf9d4
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2024-04-22 15:02:31 -0300

    feat: Se agregala experiencia de trabajo oficial

--- src/components/Lista_Punteada/Lista_Punteada.tsx
+++ src/components/Lista_Punteada/Lista_Punteada.tsx
@@ -7,17 +7,41 @@ const Lista_Punteada = () => {
     <>
       <section id="experiencia" className="experiencia">
         <div>
-          <h3 className="experiencia-title">WORK EXPERIENCE</h3>
+          <h3 className="experiencia-title">EXPERIENCIA DE TRABAJO</h3>
+
+          {/* # 1 */}
           <Lista_Punteada_Contenedor_Item
-            posicion_prop="Docente en Programacion Front End"
-            duracion_prop="Abril 2022 - Abril 2023"
-            empresa_prop="Plataforma 5"
+            posicion_prop="Desarrolladora Front End & Analista Funcional"
+            duracion_prop="Noviembre 2023 - Actualidad"
+            empresa_prop="Gobierno de la Ciudad de Buenos Aires"
           />
 
+          {/* # 2 */}
           <Lista_Punteada_Contenedor_Item
             posicion_prop="Tutora en Programacion Front End & Back End"
             duracion_prop="Abril 2023 - Diciembre 2023"
             empresa_prop="Universidad Tres de Febrero"
+          />
+
+          {/* # 3 */}
+          <Lista_Punteada_Contenedor_Item
+            posicion_prop="Tutora en Programacion Back End"
+            duracion_prop="Mayo 2023 - Diciembre 2023"
+            empresa_prop="Universidad Tres de Febrero"
+          />
+
+          {/* # 4 */}
+          <Lista_Punteada_Contenedor_Item
+            posicion_prop="Docente en Programacion Front End"
+            duracion_prop="Abril 2022 - Abril 2023"
+            empresa_prop="Plataforma 5 - Coding Bootcamp"
+          />
+
+          {/* # 5 */}
+          <Lista_Punteada_Contenedor_Item
+            posicion_prop="Desarrolladora Web Js"
+            duracion_prop="Enero 2021 - Abril 2022"
+            empresa_prop="Desarrolladora Web Freelancer"
           />
         </div>
       </section>
