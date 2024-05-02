52e7a11defed9e3724c1c80bedf5a6950ad63bd2
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2024-05-01 21:51:40 -0300

    feat: Se hace una primera configuracion del router

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import "./App.css";
-// import Lista_Punteada from "./components/Lista_Punteada/Lista_Punteada";
-// import ALTERNATIVA_Lista_Punteada from "./components/Lista_Punteada/ALTERNATIVA-Lista_Punteada/ALTERNATIVA-Lista_Punteada";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Experiencia_De_Trabajo from "./pages/Experiencia_De_Trabajo";
 import Header from "./components/Header/Header";
 
 function App() {
   return (
     <>
-      {/* <ALTERNATIVA_Lista_Punteada /> */}
-      {/* <Lista_Punteada /> */}
-      <Header/>
-      <Experiencia_De_Trabajo/>
-      {/* <ALTERNATIVA_Lista_Punteada/> */}
+      <div className="App">
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/experiencia" element={<Experiencia_De_Trabajo />} />
+          </Routes>
+            <Experiencia_De_Trabajo />
+        </BrowserRouter>
+      </div>
     </>
   );
 }
