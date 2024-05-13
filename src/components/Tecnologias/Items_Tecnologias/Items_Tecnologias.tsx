7cb50681e86532f27c3fd721924a4a6912c2f43d
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2024-05-13 12:19:16 -0300

    style: :lipstick: Se modifican los estilos de tecnologias

--- src/components/Tecnologias/Items_Tecnologias/Items_Tecnologias.tsx
+++ src/components/Tecnologias/Items_Tecnologias/Items_Tecnologias.tsx
@@ -11,7 +11,7 @@ import {
   faNode,
   faNpm,
 } from "@fortawesome/free-brands-svg-icons";
-import { faLeaf, faDatabase } from '@fortawesome/free-solid-svg-icons'
+import { faLeaf, faDatabase } from "@fortawesome/free-solid-svg-icons";
 
 const Items_Tecnologias = () => {
   return (
@@ -19,23 +19,23 @@ const Items_Tecnologias = () => {
       <article>
         <h3>Front End</h3>
         <ul>
-          <li className="dev_css" >
+          <li className="dev_css">
             <FontAwesomeIcon className="dev css" icon={faCss3} />
             <span> CSS </span>
           </li>
-          <li className="dev_html" >
+          <li className="dev_html">
             <FontAwesomeIcon className="dev html" icon={faHtml5} />
             <span> HTML </span>
           </li>
-          <li className="dev_js" >
+          <li className="dev_js">
             <FontAwesomeIcon className="dev js" icon={faJsSquare} />
             <span> JavaScript </span>
           </li>
-          <li className="dev_react" >
+          <li className="dev_react">
             <FontAwesomeIcon className="dev react" icon={faReact} />
             <span> React </span>
           </li>
-          <li className="dev_sass" >
+          <li className="dev_sass">
             <FontAwesomeIcon className="dev sass" icon={faSass} />
             <span> Sass </span>
           </li>
@@ -45,15 +45,15 @@ const Items_Tecnologias = () => {
       <article>
         <h3>Back End</h3>
         <ul>
-          <li className="dev_node" >
+          <li className="dev_node">
             <FontAwesomeIcon className="dev node" icon={faNode} />
             <span> Node </span>
           </li>
-          <li className="dev_mongo" >
+          <li className="dev_mongo">
             <FontAwesomeIcon className="dev mongo" icon={faLeaf} />
             <span> Mongo DB </span>
           </li>
-          <li className="dev_mysql" >
+          <li className="dev_mysql">
             <FontAwesomeIcon className="dev mysql" icon={faDatabase} />
             <span> MySQL</span>
           </li>
@@ -63,15 +63,15 @@ const Items_Tecnologias = () => {
       <article>
         <h3>Herramientas</h3>
         <ul>
-          <li className="dev_" >
+          <li className="dev_git">
             <FontAwesomeIcon className="dev git" icon={faGitAlt} />
             <span> Git </span>
           </li>
-          <li className="dev_" >
+          <li className="dev_github">
             <FontAwesomeIcon className="dev github" icon={faGithub} />
             <span> Github </span>
           </li>
-          <li className="dev_" >
+          <li className="dev_npm">
             <FontAwesomeIcon className="dev npm" icon={faNpm} />
             <span> NPM </span>
           </li>
