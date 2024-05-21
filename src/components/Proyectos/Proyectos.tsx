fc61ee991a7953a6de402c27cbaac53e9912f35a
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2024-05-21 16:40:16 -0300

    style: :lipstick: Se empiezan los estilos de Proyectos. Se envuelven en un <div> y se cambia cada proyecto por un <article>

--- src/components/Proyectos/Proyectos.tsx
+++ src/components/Proyectos/Proyectos.tsx
@@ -2,42 +2,44 @@ import Proyectos_Lista from "./Proyectos_Lista/Proyectos_Lista";
 
 const Proyectos = () => {
   return (
-    <section>
+    <section id="proyectos" >
       <h2>TECNOLOGIAS</h2>
       <div>
-        {/* Proyecto 1 */}
-        <Proyectos_Lista
-          project_link={"project_link"}
-          project_description={"project_description"}
-          project_title={"project_title"}
-        />
-      </div>
+        <article>
+          {/* Proyecto 1 */}
+          <Proyectos_Lista
+            project_link={"project_link"}
+            project_description={"project_description"}
+            project_title={"project_title"}
+          />
+        </article>
 
-      {/* Proyecto 2 */}
-      <div>
-        <Proyectos_Lista
-          project_link={"project_link"}
-          project_description={"project_description"}
-          project_title={"project_title"}
-        />
-      </div>
+        {/* Proyecto 2 */}
+        <article>
+          <Proyectos_Lista
+            project_link={"project_link"}
+            project_description={"project_description"}
+            project_title={"project_title"}
+          />
+        </article>
 
-      {/* Proyecto 3 */}
-      <div>
-        <Proyectos_Lista
-          project_link={"project_link"}
-          project_description={"project_description"}
-          project_title={"project_title"}
-        />
-      </div>
+        {/* Proyecto 3 */}
+        <article>
+          <Proyectos_Lista
+            project_link={"project_link"}
+            project_description={"project_description"}
+            project_title={"project_title"}
+          />
+        </article>
 
-      {/* Proyecto 4 */}
-      <div>
-        <Proyectos_Lista
-          project_link={"project_link"}
-          project_description={"project_description"}
-          project_title={"project_title"}
-        />
+        {/* Proyecto 4 */}
+        <article>
+          <Proyectos_Lista
+            project_link={"project_link"}
+            project_description={"project_description"}
+            project_title={"project_title"}
+          />
+        </article>
       </div>
     </section>
   );
