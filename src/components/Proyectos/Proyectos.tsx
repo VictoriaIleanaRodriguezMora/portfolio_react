3da63be299c7c2045124bee60227d63d759edd2d
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2024-05-21 16:32:38 -0300

    refactor: :recycle: Se le da una estructura HTML a Proyectos

--- src/components/Proyectos/Proyectos.tsx
+++ src/components/Proyectos/Proyectos.tsx
@@ -5,6 +5,34 @@ const Proyectos = () => {
     <section>
       <h2>TECNOLOGIAS</h2>
       <div>
+        {/* Proyecto 1 */}
+        <Proyectos_Lista
+          project_link={"project_link"}
+          project_description={"project_description"}
+          project_title={"project_title"}
+        />
+      </div>
+
+      {/* Proyecto 2 */}
+      <div>
+        <Proyectos_Lista
+          project_link={"project_link"}
+          project_description={"project_description"}
+          project_title={"project_title"}
+        />
+      </div>
+
+      {/* Proyecto 3 */}
+      <div>
+        <Proyectos_Lista
+          project_link={"project_link"}
+          project_description={"project_description"}
+          project_title={"project_title"}
+        />
+      </div>
+
+      {/* Proyecto 4 */}
+      <div>
         <Proyectos_Lista
           project_link={"project_link"}
           project_description={"project_description"}
--- src/components/Proyectos/Proyectos_Lista/Proyectos_Item/Proyectos_Item.tsx
+++ src/components/Proyectos/Proyectos_Lista/Proyectos_Item/Proyectos_Item.tsx
@@ -5,20 +5,20 @@ const Proyectos_Item: React.FC<Interface_Proyecto_Item> = ({
   project_link,
   project_description,
   project_title,
-//   project_icons,
-//   project_item_className
+  //   project_icons,
+  //   project_item_className
 }) => {
   return (
     // {}
     <li>
       <div>
-        <a href={project_link}> {project_link} </a>
+        <a href={project_link}>
+          <h4>{project_title}</h4>
+          <p>{project_description}</p>
 
-        {project_title}
-        {project_description}
-
-        {/* {project_icons} */}
-        {/* <FontAwesomeIcon className={`dev ${project_item_className}`} icon={project_icons} /> */}
+          {/* {project_icons} */}
+          {/* <FontAwesomeIcon className={`dev ${project_item_className}`} icon={project_icons} /> */}
+        </a>
 
       </div>
     </li>
