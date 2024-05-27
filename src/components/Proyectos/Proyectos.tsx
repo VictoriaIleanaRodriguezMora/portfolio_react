f5dc2699081f11e9cae4762bef8ea89b633fc7bb
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2024-05-27 11:50:24 -0300

    refactor: :recycle: Se pone el título de los Proyectos. Se pasa primero la prop project_title antes que la project_description

--- src/components/Proyectos/Proyectos.tsx
+++ src/components/Proyectos/Proyectos.tsx
@@ -20,8 +20,8 @@ const Proyectos = () => {
         <article>
           {/* Proyecto 1 */}
           <Proyectos_Lista
+            project_title={"Marvel"}
             project_description={"project_description"}
-            project_title={"project_title_M"}
             project_icon={[faHtml5, faCss3, faSass, faJsSquare]}
             project_github_link={"https://github.com/VictoriaIleanaRodriguezMora/Marvell.git"}
             project_deploy_link="https://marvell.vercel.app/"
@@ -32,8 +32,8 @@ const Proyectos = () => {
         {/* Proyecto 2 */}
         <article>
           <Proyectos_Lista
+            project_title={"React Ecommerce"}
             project_description={"project_description"}
-            project_title={"project_title_r_e"}
             project_icon={[faCss3, faSass, faReact]}
             project_github_link={"https://github.com/VictoriaIleanaRodriguezMora/react-ecommerce.git"}
             project_deploy_link="https://react-ecommerce-virm.vercel.app/"
@@ -44,8 +44,8 @@ const Proyectos = () => {
         {/* Proyecto 3 */}
         <article>
           <Proyectos_Lista
+            project_title={"Back End"}
             project_description={"project_description"}
-            project_title={"project_title_b_n"}
             project_icon={[faSass, faNode, faLeaf]}
             project_github_link={"https://github.com/VictoriaIleanaRodriguezMora/BackEndNode.git"}
             project_item_className={["sass", "node", "mongo"]}
@@ -55,8 +55,8 @@ const Proyectos = () => {
         {/* Proyecto 4 */}
         <article>
           <Proyectos_Lista
-            project_description={"project_description"}
-            project_title={"project_title_sql"}
+            project_title={"Base de Datos"}
+            project_description={"Pensado y desarrollado en base a las necesidades de un torneo deportivo."}
             project_icon={[faDatabase]}
             project_github_link={"https://github.com/VictoriaIleanaRodriguezMora/sql_torneo_deportivo.git"}
             project_item_className={["mysql"]}
