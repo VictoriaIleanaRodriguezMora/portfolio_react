20cb1dd4c648ea04331d18fbef4319db62ee5e59
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2024-06-05 12:58:07 -0300

    refactor: :recycle: Se pone el contenido en 'Experiencia de trabajo'

--- src/components/Lista_Punteada/Lista_Punteada.tsx
+++ src/components/Lista_Punteada/Lista_Punteada.tsx
@@ -13,7 +13,7 @@ const Lista_Punteada = () => {
             posicion_prop="Desarrolladora Front End & Analista Funcional"
             duracion_prop="NOV 2023 - Actualidad"
             empresa_prop="Gobierno de la Ciudad de Buenos Aires"
-            contenidos_prop={[""]}
+            contenidos_prop={["Cómo Analista Funcional mi rol es evaluar y comprender los requerimientos técnicos y funcionales de cada proyecto, crear y mantener documentación.", "Cómo Desarrolladora Front End trabajo en conjunto con un equipo de desarrolladores, los cúales le damos soporte, mantenimiento y nuevas funcionalidades a la Librería 'Obelisco' (Open Source)."]}
           />
 
           {/* # 2 */}
@@ -21,7 +21,7 @@ const Lista_Punteada = () => {
             posicion_prop="Tutora en Programacion Front End & Back End"
             duracion_prop="ABR 2023 - DIC 2023"
             empresa_prop="Universidad Nacional Tres de Febrero"
-            contenidos_prop={[""]}
+            contenidos_prop={["Responsable de corregir los proyectos finales de mis estudiantes en 3 comisiones a cargo.", "Responder dudas fuera del horario de clase.", "Participar activamente en las clases en vivo."]}
           />
 
           {/* # 3 */}
@@ -29,7 +29,7 @@ const Lista_Punteada = () => {
             posicion_prop="Tutora en Programacion Back End"
             duracion_prop="MAYO 2023 - DIC 2023"
             empresa_prop="Coder House"
-            contenidos_prop={[""]}
+            contenidos_prop={["Responsable de corregir semana a semana las entregas de mis estudiantes a cargo.", "Responder dudas fuera del horario de clase.", "Participar activamente en las clases en vivo."]}
           />
 
           {/* # 4 */}
@@ -37,7 +37,7 @@ const Lista_Punteada = () => {
             posicion_prop="Docente en Programacion Front End"
             duracion_prop="ABR 2022 - ABR 2023"
             empresa_prop="Plataforma 5 - Coding Bootcamp"
-            contenidos_prop={[""]}
+            contenidos_prop={["A cargo del dictado de las clases de HTML, CSS y JavaScript."]}
           />
 
           {/* # 5 */}
@@ -45,7 +45,7 @@ const Lista_Punteada = () => {
             posicion_prop="Desarrolladora Web Js"
             duracion_prop="ENE 2021 - ABR 2022"
             empresa_prop="Desarrolladora Web Freelancer"
-            contenidos_prop={[""]}
+            contenidos_prop={["Desarrolladora Web Freelancer."]}
           />
         </div>
       </section>
