0c22181f81f504edbda99993e525c7e60037792c
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2024-06-27 16:42:22 -0300

    feat: :sparkles: Se pone el contenido de Sobre Mi
    Faltan estilos

--- src/components/Sobre_Mi/Sobre_Mi.tsx
+++ src/components/Sobre_Mi/Sobre_Mi.tsx
@@ -1,10 +1,31 @@
-
-import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+("@fortawesome/free-solid-svg-icons");
 
 const Sobre_Mi = () => {
   return (
-    <div>Sobre_Mi</div>
-  )
-}
+    <div>
+      <h1>Hola! 👋🏼 Soy Victoria Rodriguez - Full Stack Web Developer 🖥️</h1>
 
-export default Sobre_Mi
+      <p>
+        {`
+        const knowMe = {
+        ImFrom: '🇦🇷 Buenos Aires, Argentina',
+        ProgrammingLanguages: ['HTML', 'CSS', 'SASS', 'JavaScript', 'React', 'Mongo DB', 'Mysql', 'Node.js', 'Git', 'Github', 'NPM'];
+        StudyingProgramming: 'Since 2019',
+        EnglishCambridge: 'B1 (still studying)',
+        LoveDogs: 'true 🐶'
+        }`}
+
+        <ul>
+          <li> <FontAwesomeIcon icon={faEnvelope} /> </li>
+          <li> <a href="https://github.com/VictoriaIleanaRodriguezMora"> <FontAwesomeIcon icon={faGithub}  /> </a> </li>
+          <li> <a href="https://www.linkedin.com/in/victoriarodriguezmora/"> <FontAwesomeIcon icon={faLinkedin}  /> </a> </li>
+        </ul>
+      </p>
+    </div>
+  );
+};
+
+export default Sobre_Mi;
